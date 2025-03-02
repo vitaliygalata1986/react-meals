404cc58f617cf4a4d440b4ce455fbda82fda9dd8
vitaliy <nertis44@gmail.com>
2025-03-02 15:50:12 +0200

    finish develop project

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,16 +1,16 @@
 function Footer() {
   return (
-    <footer className="page-footer green lighten-4">
-      <div className="footer-copyright">
-        <div className="container">
+    <footer className='page-footer green lighten-4'>
+      <div className='footer-copyright'>
+        <div className='container'>
           © {new Date().getFullYear()} Copyright Text
           <a
-            className="grey-text text-lighten-4 right"
-            href="https://github.com/vitaliygalata1986/react-meals/"
-            rel="noreferrer"
-            target="_blank"
+            className='grey-text text-lighten-4 right'
+            href='https://github.com/vitaliygalata1986/react-meals/'
+            rel='noreferrer'
+            target='_blank'
           >
-            Repo
+            Link to GitHub repository
           </a>
         </div>
       </div>
